d4b80c1731e3dd696fcec50d0ed2e5dc2c03d5b9
Gregory Romantsov <imoonserk@gmail.com>
2021-05-01 20:11:05 +0400

    add exercise 3.5: Phonebook backend step5

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json())
 let persons = [
     {
       "name": "Arto Hellas",
@@ -33,6 +34,15 @@ app.get("/api/persons", (req, res) => {
     res.json(persons);
 });
 
+app.post("/api/persons", (req, res) => {
+    const person = req.body
+    console.log(person)
+    person.id = Math.floor(Math.random() * 10000000000000);
+    persons = persons.concat(person)
+    console.log(person)
+    res.json(person)
+})
+
 app.get("/api/persons/:id", (req, res) => {
     const id = Number(req.params.id)
     const person = persons.find(person => person.id === id)
@@ -41,17 +51,17 @@ app.get("/api/persons/:id", (req, res) => {
     }else{
         res.status(404).end();
     }
-})
+});
 
 app.delete("/api/persons/:id", (req, res) => {
     const id = Number(req.params.id)
     persons = persons.filter(person => person.id !== id)
 
     res.status(204).end()
-})
+});
 
 const PORT = 3005;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-})
+});
